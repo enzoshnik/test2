be283501b10ed9f491e25c99a8cf0db6b20a0465
enzoshnik <enzoshnik@gmail.com>
2015-03-17 12:29:52 +0300

    second commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("keywords_inner", "веб-студия, web-studio, web-студия, сайт, создание, разработка, поддержка, продвижение");
+$APPLICATION->SetPageProperty("keywords", "веб-студия, web-studio, web-студия, сайт, создание, разработка, поддержка, продвижение");
+$APPLICATION->SetPageProperty("title", "EnzoLab - студия web-студия, разработка и оптимизация сайтов");
+$APPLICATION->SetPageProperty("description", "▶ EnzoLab - web-студия, где работают люди, объединенные общей идеей и целью - создавать сайты правильно и не волноваться, что люди не увидят хорошие сайты.");
+$APPLICATION->SetTitle("EnzoLab - студия web-разработки");
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
